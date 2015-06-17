ce484be36215962fe66ad21f296b36aaa80cdc81
telranAndroid <telran.asana@gmail.com>
2015-06-17 08:47:04 +0300

    2.01 Add Network Call

--- app/src/main/java/com/telran/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/telran/sunshine/MainActivityFragment.java
@@ -1,12 +1,20 @@
 package com.telran.sunshine;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 /**
@@ -31,9 +39,72 @@ public class MainActivityFragment extends Fragment {
                 "Sat - Sunny - 76 / 68",
         };
 
-        ArrayAdapter<String> forecastAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_txtvw, forecastArray);
-        ListView forcastList = (ListView)rootView.findViewById(R.id.listview_forecast);
+        ArrayAdapter<String> forecastAdapter =
+                new ArrayAdapter<String>(getActivity(),
+                        R.layout.list_item_forecast,
+                        R.id.list_item_forecast_txtvw,
+                        forecastArray);
+        ListView forcastList = (ListView)rootView
+                .findViewById(R.id.listview_forecast);
         forcastList.setAdapter(forecastAdapter);
+        // These two need to be declared outside the try/catch
+        // so that they can be closed in the finally block.
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+        // Will contain the raw JSON response as a string.
+        String forecastJsonStr = null;
+
+        try {
+            // Construct the URL for the OpenWeatherMap query
+            // Possible parameters are avaiable at OWM's forecast API page, at
+            // http://openweathermap.org/API#forecast
+            URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+
+            // Create the request to OpenWeatherMap, and open the connection
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // Read the input stream into a String
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                // Nothing to do.
+                return null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                return null;
+            }
+            forecastJsonStr = buffer.toString();
+        } catch (IOException e) {
+            Log.e("MainActivityFragment", "Error ", e);
+            // If the code didn't successfully get the weather data, there's no point in attemping
+            // to parse it.
+            return null;
+        } finally{
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e("MainActivityFragment", "Error closing stream", e);
+                }
+            }
+        }
 
         return rootView;
     }
