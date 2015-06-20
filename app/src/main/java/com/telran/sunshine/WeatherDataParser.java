851f5da98c41b062195a3474d8a87c281da5aed0
telranAndroid <telran.asana@gmail.com>
2015-06-20 21:23:04 +0300

    2.08 JSON Parsing Quiz - Question

---
+++ app/src/main/java/com/telran/sunshine/WeatherDataParser.java
@@ -0,0 +1,21 @@
+package com.telran.sunshine;
+
+import org.json.JSONException;
+
+/**
+ * Created on 20-Jun-15.
+ */
+public class WeatherDataParser {
+
+    /**
+     * Given a string of the form returned by the api call:
+     * http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7
+     * retrieve the maximum temperature for the day indicated by dayIndex
+     * (Note: 0-indexed, so 0 would refer to the first day).
+     */
+    public static double getMaxTemperatureForDay(String weatherJsonStr, int dayIndex)
+            throws JSONException {
+
+        return 0;
+    }
+}
