446a80d05a8303f53745270f49cbc78459df785a
telranAndroid <telran.asana@gmail.com>
2015-06-20 12:56:22 +0300

    2.05 Execute AsyncTask (without INTERNET permission)

--- app/src/main/java/com/telran/sunshine/ForecastFragment.java
+++ app/src/main/java/com/telran/sunshine/ForecastFragment.java
@@ -86,8 +86,10 @@ public class ForecastFragment extends Fragment {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if(id==R.id.action_refresh)
+        if(id==R.id.action_refresh) {
+            new FetchWeatherTask().execute();
             return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
