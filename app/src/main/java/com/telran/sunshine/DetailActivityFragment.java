25ddab3d20f1dccf191bdd937bc1598f8eb22094
telranAndroid <telran.asana@gmail.com>
2015-06-28 00:38:53 +0300

    3.04 Display Content in DetailActivity

--- app/src/main/java/com/telran/sunshine/DetailActivityFragment.java
+++ app/src/main/java/com/telran/sunshine/DetailActivityFragment.java
@@ -1,10 +1,12 @@
 package com.telran.sunshine;
 
-import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -18,6 +20,15 @@ public class DetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        Intent parentIntent = getActivity().getIntent();
+        if(parentIntent != null && parentIntent.hasExtra(Intent.EXTRA_TEXT)) {
+            String forecast = parentIntent.getStringExtra(Intent.EXTRA_TEXT);
+            TextView tv = (TextView) rootView.findViewById(R.id.txtvw_helloworld);
+            tv.setText(forecast);
+        }
+
+        return rootView;
     }
 }
