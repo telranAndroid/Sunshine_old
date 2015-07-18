b89a43df4c3b884685677dff6bf8e1d3421aeb05
telranAndroid <telran.asana@gmail.com>
2015-07-18 16:23:54 +0300

    4.21 Fix Detail View cont. (include ShareIntent)

--- app/src/main/java/com/telran/sunshine/DetailActivityFragment.java
+++ app/src/main/java/com/telran/sunshine/DetailActivityFragment.java
@@ -1,9 +1,14 @@
 package com.telran.sunshine;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
@@ -13,17 +18,47 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CursorAdapter;
 import android.widget.TextView;
+
+import com.telran.sunshine.data.WeatherContract;
+
+import java.util.Locale;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class DetailActivityFragment extends Fragment {
+public class DetailActivityFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final String LOG_TAG = DetailActivityFragment.class.getSimpleName();
     private static final String FORECAST_SHARE_HASHTAG = "#SunshineApp";
+    private static final int LOADER_DETAILS = 0;
+
     private String mForecastStr;
+    private ShareActionProvider mShareActionProvider;
+
+    private static final String[] PROJECTION_FORECAST_COLUMNS = {
+            // In this case the id needs to be fully qualified with a table name, since
+            // the content provider joins the location & weather tables in the background
+            // (both have an _id column)
+            // On the one hand, that's annoying.  On the other, you can search the weather table
+            // using the location set by the user, which is only in the Location table.
+            // So the convenience is worth it.
+            WeatherContract.WeatherEntry.TABLE_NAME + "." + WeatherContract.WeatherEntry._ID,
+            WeatherContract.WeatherEntry.COLUMN_DATE,
+            WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
+            WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_MIN_TEMP
+    };
+
+    // These indices are tied to PROJECTION_FORECAST_COLUMNS.
+    // If PROJECTION_FORECAST_COLUMNS changes, these must change.
+    static final int WEATHER_COL_ID = 0;
+    static final int WEATHER_COL_DATE = 1;
+    static final int WEATHER_COL_DESC = 2;
+    static final int WEATHER_COL_TEMP_MAX = 3;
+    static final int WEATHER_COL_TEMP_MIN = 4;
 
     public DetailActivityFragment() {
 
@@ -69,12 +104,13 @@ public class DetailActivityFragment extends Fragment {
 
         MenuItem itemShare = menu.findItem(R.id.action_share);
 
-        ShareActionProvider shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(itemShare);
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(itemShare);
 
         // Attach an intent to this ShareActionProvider.  You can update this at any time,
         // like when the user selects a new piece of data they might like to share.
-        if (shareActionProvider != null ) {
-            shareActionProvider.setShareIntent(createShareForecastIntent());
+        if (mShareActionProvider != null ) {
+            if(mForecastStr!=null && !mForecastStr.isEmpty())
+                mShareActionProvider.setShareIntent(createShareForecastIntent());
         } else {
             Log.d(LOG_TAG, "Share Action Provider is null?");
         }
@@ -94,13 +130,131 @@ public class DetailActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
 
+        return rootView;
+    }
+
+    /**
+     * Called when the fragment's activity has been created and this
+     * fragment's view hierarchy instantiated.  It can be used to do final
+     * initialization once these pieces are in place, such as retrieving
+     * views or restoring state.  It is also useful for fragments that use
+     * {@link #setRetainInstance(boolean)} to retain their instance,
+     * as this callback tells the fragment when it is fully associated with
+     * the new activity instance.  This is called after {@link #onCreateView}
+     * and before {@link #onViewStateRestored(Bundle)}.
+     *
+     * @param savedInstanceState If the fragment is being re-created from
+     *                           a previous saved state, this is the state.
+     */
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        getLoaderManager().initLoader(LOADER_DETAILS, null, this);
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    /**
+     * Instantiate and return a new Loader for the given ID.
+     *
+     * @param id   The ID whose loader is to be created.
+     * @param args Any arguments supplied by the caller.
+     * @return Return a new Loader instance that is ready to start loading.
+     */
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+
+        Loader<Cursor> loader = null;
+
         Intent parentIntent = getActivity().getIntent();
         if(parentIntent != null) {
-            mForecastStr = parentIntent.getDataString();
-            TextView tv = (TextView) rootView.findViewById(R.id.txtvw_helloworld);
-            tv.setText(mForecastStr);
+            loader = new android.support.v4.content.CursorLoader(
+                    getActivity(),
+                    parentIntent.getData(),
+                    PROJECTION_FORECAST_COLUMNS,
+                    null,
+                    null,
+                    null
+            );
+            Log.v(LOG_TAG, "In onCreateLoader(...);");
+        }
+        return loader;
+    }
+
+    /**
+     * Called when a previously created loader has finished its load.  Note
+     * that normally an application is <em>not</em> allowed to commit fragment
+     * transactions while in this call, since it can happen after an
+     * activity's state is saved.  See {@link *FragmentManager#beginTransaction()
+     * FragmentManager.openTransaction()} for further discussion on this.
+     * <p/>
+     * <p>This function is guaranteed to be called prior to the release of
+     * the last data that was supplied for this Loader.  At this point
+     * you should remove all use of the old data (since it will be released
+     * soon), but should not do your own release of the data since its Loader
+     * owns it and will take care of that.  The Loader will take care of
+     * management of its data so you don't have to.  In particular:
+     * <p/>
+     * <ul>
+     * <li> <p>The Loader will monitor for changes to the data, and report
+     * them to you through new calls here.  You should not monitor the
+     * data yourself.  For example, if the data is a {@link Cursor}
+     * and you place it in a {@link CursorAdapter}, use
+     * the {@link CursorAdapter#CursorAdapter(Context,
+     * Cursor, int)} constructor <em>without</em> passing
+     * in either {@link CursorAdapter#FLAG_AUTO_REQUERY}
+     * or {@link CursorAdapter#FLAG_REGISTER_CONTENT_OBSERVER}
+     * (that is, use 0 for the flags argument).  This prevents the CursorAdapter
+     * from doing its own observing of the Cursor, which is not needed since
+     * when a change happens you will get a new Cursor throw another call
+     * here.
+     * <li> The Loader will release the data once it knows the application
+     * is no longer using it.  For example, if the data is
+     * a {@link Cursor} from a {@link CursorLoader},
+     * you should not call close() on it yourself.  If the Cursor is being placed in a
+     * {@link CursorAdapter}, you should use the
+     * {@link CursorAdapter#swapCursor(Cursor)}
+     * method so that the old Cursor is not closed.
+     * </ul>
+     *
+     * @param loader The Loader that has finished.
+     * @param data   The data generated by the Loader.
+     */
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+        if(data.moveToFirst()) {
+            Log.v(LOG_TAG, "In onLoadFinished(...)");
+
+            String strDate = Utility.formatDate(data.getLong(WEATHER_COL_DATE));
+            String strDesc = data.getString(WEATHER_COL_DESC);
+            Boolean isMetric = Utility.isMetric(getActivity());
+            String strTempMax = Utility.formatTemperature(data.getDouble(WEATHER_COL_TEMP_MAX), isMetric);
+            String strTempMin = Utility.formatTemperature(data.getDouble(WEATHER_COL_TEMP_MIN), isMetric);
+
+            mForecastStr = String.format(Locale.getDefault(), "%s - %s - %s / %s",
+                    strDate,
+                    strDesc,
+                    strTempMax,
+                    strTempMin);
+
+            TextView txtVw = (TextView) getView().findViewById(R.id.txtvw_helloworld);
+            txtVw.setText(mForecastStr);
+
+            // If Options Menu initialised we can set its ShareIntent
+            if (mShareActionProvider != null && mForecastStr!=null && !mForecastStr.isEmpty())
+                mShareActionProvider.setShareIntent(createShareForecastIntent());
         }
 
-        return rootView;
+    }
+
+    /**
+     * Called when a previously created loader is being reset, and thus
+     * making its data unavailable.  The application should at this point
+     * remove any references it has to the Loader's data.
+     *
+     * @param loader The Loader that is being reset.
+     */
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
